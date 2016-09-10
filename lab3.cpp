bdb47df8d989e209ffc270a3f786e28960edb8a2
JERIC MALCO <JMMALCO@fit.edu.ph>
2016-09-10 11:37:18 +0800

    Add custon strcpy

--- lab3.cpp
+++ lab3.cpp
@@ -2,13 +2,31 @@
 #include<cstring>
 using namespace std;
 
-int myStrCmp(char* str1,char* str2){
-	return strcmp(str1,str2);
+char* myStrCmp(char* str1,char* str2){
+	
+	switch(strcmp(str1, str2)){
+		case -1:
+			cout<<"negative";
+			break;
+		case 0:
+			cout<<"equal";
+			break;
+		case 1:
+		cout<<"positive";
+			break;
+	}
+	
+}
+
+void myStrCpy(char* str1,char* str2){
+	strcpy(str1, str2);
 }
 
 int main(){
-	char str1[100]="hello";
-	char str2[100]="Hello";
+	char str1[100]="Hello";
+	char str2[100]="hello";
 	
-	cout<<myStrCmp(str1,str2);
+	myStrCmp(str1,str2);
+	myStrCpy(str1,str2);
+	cout<<str1;
 }
