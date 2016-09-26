56b1d9f21d27b214156be8425d1fa5a1ab7b5a25
jericmalco <jrc4827@gmail.com>
2016-09-26 10:59:56 +0800

    Update lab2.cpp

--- lab2.cpp
+++ lab2.cpp
@@ -1,19 +1,45 @@
-// Create a program that will generate 10 unique numbers ranging from 1 to 20
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
-
+// Create a program compute the area of the following polygons and circle
+#include<iostream>
+#include<cmath>
 using namespace std;
-
 int main()
 {
-	srand((int)time(0));
-	int i = 0;
-	while(i++ < 10) 
+	int choice;
+	cout<<"*************"<<endl<<"MENU"<<endl<<"*************"<<endl;
+	cout<<"[1] - Area of Square"<<endl;
+	cout<<"[2] - Area of Rectangle"<<endl;
+	cout<<"[3] - Area of Triangle"<<endl;
+	cout<<"[4] - Area of Circle"<<endl;
+	cout<<"[5] - Exit"<<endl;
+	cout<<"-----------------"<<endl;
+	cout<<"Enter Your Choice: ";
+	cin>> choice;
+	cout<<"-----------------"<<endl;
+	switch(choice)
 	{
-		int r = (rand() % 20) + 1;
-		cout << r << " ";
+	double l,w,h,b,r,ans;
+	case 1:cout<< "AREA OF SQUARE"<< endl<<"-----------------"<<endl<<"Enter the side of the square: ";
+		   cin>>l;
+		   cout<<endl;
+		   ans=pow(l,2);
+		   cout<<"The Area of the square is "<<ans<<" sq. units"<<endl;break;
+	case 2:cout<< "AREA OF RECTANGLE"<< endl<<"-----------------"<<endl<<"Enter the length and width of the rectangle: ";
+		   cin>>l >>w;
+		   cout<<endl;
+		   ans=l*w;
+		   cout<<"The Area of the rectangle is "<<ans<<" sq. units"<<endl;break;
+	case 3:cout<< "AREA OF TRIANGLE"<< endl<<"-----------------"<<endl<<"Enter the base and the width of the height: ";
+		   cin>>b >>h;
+		   cout<<endl;
+		   ans=(b*h)/2;
+		   cout<<"The Area of the triangle is "<<ans<<" sq. units"<<endl;break;
+	case 4:cout<< "AREA OF CIRCLE"<< endl<<"-----------------"<<endl<<"Enter the radius: ";
+		   cin>>r;
+		   cout<<endl;
+		   ans=3.14*pow(r,2);
+		   cout<<"The Area of the circle is "<<ans<<" sq. units"<<endl;break;
+	case 5:cout<<"Thank you!"<<endl;
+	  
 	}
-	system("pause>0");
-	return 0;
+	system("pause");
 }
